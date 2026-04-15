ed4220b7e8cbea127e47df241cb430c1c0d8073b
Andreu Ortega Blasi <aortegablasi@gmail.com>
2026-04-15 17:39:03 +0200

    Added and completed Week 5 'autogen' project

--- 5_autogen/autogen_project/world.py
+++ 5_autogen/autogen_project/world.py
@@ -6,15 +6,26 @@ from autogen_core import AgentId
 import messages
 import asyncio
 
-HOW_MANY_AGENTS = 5
+HOW_MANY_AGENTS = 3
+HOW_MANY_CREATOR_AGENTS = 3
 
-async def create_and_message(worker, creator_id, i: int):
+async def create_and_message(worker, creator_id, i, z: int):
     try:
-        result = await worker.send_message(messages.Message(content=f"agent{i}.py"), creator_id)
-        with open(f"idea{i}.md", "w") as f:
+        result = await worker.send_message(messages.Message(content=f"agent{i}_{z}.py"), creator_id)
+        with open(f"idea{i}_{z}.md", "w") as f:
             f.write(result.content)
     except Exception as e:
         print(f"Failed to run worker {i} due to exception: {e}")
+
+async def create_creator_and_message(worker,creator_id, i:int):
+    try:
+        agent_name = await worker.send_message(messages.Message(content=f"creator{i}.py"), creator_id)
+        new_creator_id = AgentId(agent_name.content, "default")
+        coroutines = [create_and_message(worker, new_creator_id, i, z) for z in range(1, HOW_MANY_AGENTS+1)]
+        await asyncio.gather(*coroutines)
+    except Exception as e:
+        print(f"Failed to run creator {i} due to exception: {e}")
+
 
 async def main():
     host = GrpcWorkerAgentRuntimeHost(address="localhost:50051")
@@ -23,13 +34,14 @@ async def main():
     await worker.start()
     result = await Creator.register(worker, "Creator", lambda: Creator("Creator"))
     creator_id = AgentId("Creator", "default")
-    coroutines = [create_and_message(worker, creator_id, i) for i in range(1, HOW_MANY_AGENTS+1)]
+    coroutines = [create_creator_and_message(worker, creator_id, i) for i in range(1, HOW_MANY_CREATOR_AGENTS+1)]
     await asyncio.gather(*coroutines)
     try:
         await worker.stop()
         await host.stop()
     except Exception as e:
         print(e)
+
 
 
 
